60b127f85b8a3f851a2451dddd69921b02a87c79
Raimundo Alexandre <alexandre.fsantos08@gmail.com>
2025-08-05 19:32:20 -0300

    Passo inicial do projeto

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+console.log(amigos);
+function adicionarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    if (amigo == "") {
+        alert("Insira um amigo.");
+        return;
+    }
+    amigos.push(amigo);
+    document.getElementById("amigo").value = "";
+    atualizarLista();
+}       
